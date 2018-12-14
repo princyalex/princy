aefa55a13946435b7febfb1afee4ce6947c6d944
princyalex <41040391+princyalex@users.noreply.github.com>
2018-12-14 12:06:31 +0530

    Create register.js

---
+++ register.js
@@ -0,0 +1,50 @@
+Function Validate form(){
+x=document get Element By ID("user name")value;
+if(x"=="){
+alert("user name must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("college name")value;
+if(x"=="){
+alert("college name must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("degree")value;
+if(x"=="){
+alert("degree must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("language")value;
+if(x"=="){
+alert("language must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("email")value;
+if(x"=="){
+alert("email must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("password")value;
+if(x"=="){
+alert("passwordx must be filled");
+return false;
+}
+}
+Function Validate form(){
+x=document get Element By ID("repeat password")value;
+if(x"=="){
+alert("repeat password must be filled");
+return false;
+}
+}
+}
