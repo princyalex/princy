76e62ac2f020537b2694a49cfdec8a792f9f3536
princyalex <41040391+princyalex@users.noreply.github.com>
2018-12-14 12:07:41 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,11 @@
+Function Validate form(){
+x=document get Element By ID("email")value;
+if(x"=="){
+alert("email must be filled");
+return false;
+}Function Validate form(){
+x=document get Element By ID("password")value;
+if(x"=="){
+alert("password must be filled");
+return false;
+}
